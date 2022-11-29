43e2d6c6f882c126dbcff3f8aa81a24d44862aef
Ziping Liu <dev@ziping.life>
2022-11-29 06:45:00 -0600

    use tsc for now cuz i'm an idiot

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,69 @@
 
-import React from 'react';
-
+import React, {Component, useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 import lodash from 'lodash';
-import PreviewFileConfig from "./@types/PreviewFIleConfig";
+
+export type PreviewFileConfig = {
+  showZoomControl: boolean;
+  showAnnotationTools: boolean;
+  showFullScreen: boolean;
+  /**
+     * This variable takes a string value of "FIT_WIDTH", "FIT_PAGE", "TWO_COLUMN" or "TWO_COLUMN_FIT_PAGE".
+        1. FIT_WIDTH: Expands the page horizontally to the full width of the document pane.
+        2. FIT_PAGE: Displays the entire page in the current view pane.
+        3. TWO_COLUMN: Displays two pages of the PDF side by side in the current view pane.
+        4. TWO_COLUMN_FIT_PAGE: Displays two pages of the PDF side by side where the entire two pages are displayed in 
+        the current view pane. Note that end users can also toggle the view mode via the Fit Width, Fit Page 
+        or Two-Column button on the right-hand panel.
+
+        In addition to these, there are two other view modes which are supported only in mobile browsers:
+        1. CONTINUOUS: This mode displays all the document pages one after the other and users can easily navigate through the pages by scrolling up or down.
+        2. SINGLE_PAGE: This mode displays only a single document page at a time and doesn’t show any adjoining page. 
+        Users can use the swipe gesture to navigate to other pages which will be displayed one at a time.
+     */
+  defaultViewMode:
+  | "FIT_WIDTH"
+  | "FIT_PAGE"
+  | "TWO_COLUMN"
+  | "TWO_COLUMN_FIT_PAGE"
+  | "CONTINUOUS"
+  | "SINGLE_PAGE";
+  enableFormFilling: boolean;
+  showDownloadPDF: boolean;
+  showPrintPDF: boolean;
+  showLeftHandPanel: boolean;
+  showPageControls: boolean;
+
+  /**
+   * The top bar in lightbox embed mode contains the close button by default to
+   * close the PDF preview which can be configured to Back button by setting exitPDFViewerType to "RETURN".
+   */
+  exitPDFViewerType: "CLOSE" | "BACK";
+  showThumbnails: boolean;
+  showBookmarks: boolean;
+  /**
+   * Set this to true to enable PDF linearization. For more details, see the section PDF linearization.
+   */
+  enableLinearization: boolean;
+  /**
+   * Set this to true to add, update and delete PDF annotations programmatically in full window embed mode.
+   * For more details, see the section Annotations API overview.
+   * https://developer.adobe.com/document-services/docs/overview/pdf-embed-api/howtos_comments/#annotations-api-overview
+   */
+  enableAnnotationAPIs: boolean;
+  /** Leave blank for default as FULL WINDOW MODE
+   * https://documentservices.adobe.com/view-sdk-demo/index.html#/view/FULL_WINDOW/Bodea%20Brochure.pdf
+   */
+  embedMode: "LIGHT_BOX" | "SIZED_CONTAINER" | "IN_LINE" | string;
+  enableSearchAPIs: boolean;
+  showDisabledSaveButton: boolean;
+  /**
+   * With this configuration, website developers have the flexibility to control if the
+   * PDF should take focus when it is rendered. For more details, see the section Focus on PDF rendering.
+   */
+  focusOnRendering: any;
+}
+
 
 
 
@@ -45,7 +106,7 @@ export const DefaultConfigs = {
   },
 };
 
-export class AdobeReactView extends React.Component<
+export class AdobeReactView extends Component<
   AdobeReactViewProps & Partial<HTMLDivElement>,
   Required<AdobeReactViewProps> & {
     adobeDCView: null | {
@@ -60,25 +121,20 @@ export class AdobeReactView extends React.Component<
     };
   } & Partial<HTMLDivElement>
 > {
-  static checkForViewJsLoaded() {
+   checkForViewJsLoaded() {
     return (
       (
-        globalThis as {
-          [key: string]: any;
-        }
+      window as any
       )["adobe_dc_view_sdk"] != undefined &&
-      typeof (globalThis as any)["adobe_dc_view_sdk"] === "function"
+      typeof (window as any)["adobe_dc_view_sdk"] === "function"
     );
   }
 
-  static checkForDeprecatedMainJsLoaded() {
+   checkForDeprecatedMainJsLoaded() {
     return (
       (
-        globalThis as {
-          [key: string]: any;
-        }
-      )["AdobeDC"] != undefined &&
-      typeof (globalThis as any)["AdobeDC"] === "function"
+        window as any) ["AdobeDC"] != undefined &&
+       (window as any)["AdobeDC"] === "function"
     );
   }
 
@@ -88,7 +144,7 @@ export class AdobeReactView extends React.Component<
     } & Partial<HTMLDivElement>
   ) {
     super(props);
-    const [adobeMainReady, setAdobeMainReady] = React.useState(
+    const [adobeMainReady, setAdobeMainReady] = useState(
       props.adobeMainReady || false
     );
 
@@ -109,7 +165,12 @@ export class AdobeReactView extends React.Component<
         },
       },
     };
-    const [isReady, setIsReady] = React.useState(props.isReady || false);
+
+    this.checkForViewJsLoaded = this.checkForViewJsLoaded.bind(this);
+    this.checkForDeprecatedMainJsLoaded = this.checkForDeprecatedMainJsLoaded.bind(this);
+      this.render = this.render.bind(this);
+    
+
   }
 
   previewFile = (
@@ -170,8 +231,8 @@ export class AdobeReactView extends React.Component<
     }
 
     if (
-      AdobeReactView.checkForDeprecatedMainJsLoaded() === true ||
-      AdobeReactView.checkForViewJsLoaded() === true
+      this.checkForDeprecatedMainJsLoaded() === true ||
+      this.checkForViewJsLoaded() === true
     ) {
       if (this.state.adobeMainReady === false) {
         let oldState = lodash.cloneDeep(this.state);
@@ -259,39 +320,43 @@ const ReactViewAdobe = (
     previewConfig: Partial<PreviewFileConfig>;
   } & Partial<HTMLDivElement>
 ) => {
-  const [adobeMainReady, setAdobeMainReady] = React.useState(
+  const [adobeMainReady, setAdobeMainReady] = useState(
     props.adobeMainReady || false
   );
-  const [isReady, setIsReady] = React.useState(props.isReady || false);
+  const [isReady, setIsReady] = useState(props.isReady || false);
   const divID = props.config?.divId || DefaultConfigs.staticDivId;
 
-  React.useEffect(() => {
+
+
+  const ReactView = new AdobeReactView({
+    previewConfig: props.previewConfig,
+    config: props.config,
+    isReady:isReady,
+    adobeMainReady:adobeMainReady,
+    style: props.style,
+    className: props.className,
+    tabIndex: props.tabIndex,
+    tagName:
+      props.tagName ||
+      "react-adobe-state-government-ensuring-rule-of-law-against-amazon-retaliator-or-a-child-component-framed-by-state-regulations-such-as-adobe-react-embed-core-div"
+    
+  })
+
+  useEffect(() => {
     if (document.getElementById(divID)) {
       setIsReady(true);
     }
 
     if (
-      AdobeReactView.checkForDeprecatedMainJsLoaded() === true ||
-      AdobeReactView.checkForViewJsLoaded() === true
+      ReactView.checkForDeprecatedMainJsLoaded() === true ||
+      ReactView.checkForViewJsLoaded() === true
     ) {
       setAdobeMainReady(true);
     }
-  }, [isReady, adobeMainReady, divID]);
+  }, [isReady, adobeMainReady, divID, ReactView]);
 
   return (
-    <AdobeReactView
-      isReady={isReady}
-      adobeMainReady={adobeMainReady}
-      previewConfig={props.previewConfig}
-      config={props.config}
-      style={props.style}
-      className={props.className}
-      tabIndex={props.tabIndex}
-      tagName={
-        props.tagName ||
-        "react-adobe-state-government-ensuring-rule-of-law-against-amazon-retaliator-or-a-child-component-framed-by-state-regulations-such-as-adobe-react-embed-core-div"
-      }
-    />
+    {ReactView}
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
 const path = require('path');
+const tsloader = require('ts-loader');
+const react = require('react');
 module.exports = env => {
    const envString = JSON.stringify(env, null, 2);
    const envObj = JSON.parse(envString);
-   console.log(envString, envObj['production'] )
    const fileName = envObj['production'] ? 'index.min.js' : 'index.dev.js';
    const loader = envObj['production'] ? 'babel-loader' : 'ts-loader';
-   console.log(fileName)
+
+   const loaders = {
+    'babel-loader': require.resolve('babel-loader'),
+    'ts-loader': require.resolve('ts-loader')
+   }
+   console.log('loader', loader);
    return {
     mode: 'production',
-    entry: path.join(__dirname, 'src', 'index.tsx'),
+entry: path.join(__dirname, 'src', 'index.tsx'),
     devtool: 'inline-source-map',
     output: {
         path: path.join(__dirname, 'lib'),
         filename: fileName,
+        publicPath: '/lib'
     },
     devtool: 'inline-source-map',
 
-  
-    
-    module: {
-    noParse:[
-    /node_modules\/react/
-    
-],
 
+    module: {
+ 
+        
     
         rules: [
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-                loader
+                loader: loaders[loader],
+             
             },
             {
                 test: /\.jsx?$/,
-                use: 'babel-loader',
+                loader: loaders['babel-loader'],
                 exclude: /node_modules/
                 },
         
@@ -44,10 +48,6 @@ module.exports = env => {
         extensions: ['.jsx', 'ts', 'tsx', 'js'],
         
     },
-    externals: {
-        react: 'react',
-        'react-dom': 'react-dom',
-        'react-router-dom': 'react-router-dom',
-    }
+
    }
 }
