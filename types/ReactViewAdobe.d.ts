a59fa18b2c300c3cad9c2ed46fe9d2fba62c2793
Ziping Liu <dev@ziping.life>
2023-05-25 03:02:37 -0500

    fix: remove outdated types

--- types/ReactViewAdobe.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { PreviewFileConfig } from "./util";
-import { EmbedState } from "./index";
-export declare const ReactViewAdobe: (props: Partial<EmbedState> & {
-    previewConfig: Partial<PreviewFileConfig>;
-} & Partial<Omit<HTMLDivElement, 'style'>> & {
-    style?: React.HTMLAttributes<HTMLDivElement>["style"];
-}) => JSX.Element;
