7c28f5eded456534a04aae003367a94f2c671bc8
zipingl <liuziping@gmail.com>
2023-10-28 18:30:06 -0500

    fix: remove conditional env file creation

--- scripts/public.js
+++ scripts/public.js
@@ -169,9 +169,6 @@ const htmlMiny = minify(htmlNew, {
 });
 if(process && process?.env){
   process.env.DATEMODIFIED_CODE_DEPLOYED = modifiedDate;
-} else {
-  const env_filestring = `DATEMODIFIED_CODE_DEPLOYED=${modifiedDate}`;
-  fs.writeFileSync(path.resolve(__dirname, '../.env'), env_filestring, 'utf8');
-}
-
+} 
+fs.writeFileSync(path.resolve(__dirname, '../.env'), env_filestring, 'utf8'); fs.writeFileSync(path.resolve(__dirname, '../.env'), env_filestring, 'utf8');
 fs.writeFileSync(path.resolve(__dirname, '../build/index.html'), htmlMiny, 'utf8');
