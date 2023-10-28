ade2538f26c26548364f303ced3be0dba61bf968
Ziping Liu <dev@ziping.life>
2023-10-27 23:12:37 -0500

    feat: update canary deployed app html build process

---
+++ scripts/public.js
@@ -0,0 +1,173 @@
+const fs = require('fs-extra');
+const path = require('path');
+const minify = require('html-minifier').minify;
+
+const publicIndexhtml = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf8');
+const readme = fs.readFileSync(path.resolve(__dirname, '../README.md'), 'utf8');
+
+const regexMatchHead = /<head>([\s\S]*?)<\/head>/;
+
+const headMatched = publicIndexhtml.match(regexMatchHead);
+const matchedTitle = headMatched[1].match(/<title>([\s\S]*?)<\/title>/);
+
+const regexMatchLongWords =  /[a-zA-Z]{5,}/g;
+ 
+
+const wordsarra =readme.match(regexMatchLongWords).map((word) => {
+    return word.toLowerCase();
+});
+ 
+const wordsarra2Unique =    [...new Set(wordsarra)];
+const wordAvoid =[
+    'this',
+    'which',
+    'script',
+    'com',
+    'title',
+    'great',
+    'window',
+    'meta',
+    'based',
+    'actionworfklow',
+    'action',
+    'with',
+    'tool',
+    'rich',
+    'badge',
+    'project',
+    'based',
+    'tag',
+    'e012350f',
+    'github',
+    'font',
+    'content',
+    'wakatime',
+    'style',
+    'https',
+    'http',
+    'src',
+    'of',
+    'href',
+    'gtag',
+    'for',
+    'google',
+    'dataLayer',
+    'with',
+    'stylesheet',
+    'from',
+    'enjoys',
+    'that',
+    'this',
+    'crossorigin',
+    'rel',
+    'have',
+    'been',
+    'will',
+
+    'your',
+    'about',
+
+]
+
+const wordsarra2UniqueFiltered = wordsarra2Unique.filter((word) => {
+    return !wordAvoid.includes(word);
+}   );
+
+
+const freqMapWor = wordsarra2UniqueFiltered.map((word) => {
+    return {
+        word,
+        freq: headMatched[1].split(word).length,
+    };
+}
+
+);
+
+const topTenWords = freqMapWor.sort((a, b) => b.freq - a.freq).slice(0, 12);
+console.log(topTenWords);
+const headMatchedModded = headMatched +
+`
+<meta property="og:image" content="https://blog.zi-ping.com/wp-content/uploads/2023/10/love-blue.png">
+<meta property="article:author" content="https://zi-ping.com/resume">
+<meta name="author" content="Liu, Zi-ping">
+<meta name="description" content="Testing the React Component over Time as the Continuous Form of Avaiability Monitoring Concerning the React Adobe Embed custom react component library Due to possible failiures caused by external API changes ( Adobe Embed API is complicated and changes without notice often) Due to possible failures caused by code changes to the React Component that are unable to be caught through unit tests">
+<meta property="article:published_time" content="2023-10-13T04:22:32-05:00">
+<meta property="article:modified_time" content="${new Date().toISOString()}">
+<meta property="og:updated_time" content="${new Date().toISOString()}">
+<meta property="og:title" content="${matchedTitle[1]}">
+<meta property="og:image" content="https://blog.zi-ping.com/wp-content/uploads/2023/10/love-blue.png">
+<meta name="image" content="https://blog.zi-ping.com/wp-content/uploads/2023/10/love-blue.png">
+<meta property="og:type" content="article">
+<link rel="canonical" href="https://ziping-liu-corporation.github.io/react-adobe-embed/">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+<meta property="og:url" content="https://blog.zi-ping.com/resume/">
+<script type="application/ld+json" class="ziping-liu-proprietary-technologies-scheme-graphical-json-datas-patent-pending">
+{
+    "@context": "https://schema.org",
+    "@graph": [
+      {
+        "@type": "Article",
+        "author": {
+          "name": "Ziping Liu",
+          "email": "liu@blog.zi-ping.com",
+          "url": "https://zi-ping.com/resume",
+          "@id": "https://zi-ping.com/resume"
+        },
+        "headline": "${matchedTitle[1]}",
+        "datePublished":  "2023-10-27T00:00:00+00:00",
+        "dateModified" : "${new Date().toISOString(). 
+            //replace the .000Z with offset gmtoffset
+            replace(/\.\d\d\dZ/, '+00:00')}",
+        }",
+        "mainEntityOfPage": {
+          "@id": "https://ziping-liu-corporation.github.io/react-adobe-embed/"
+        },
+        "wordCount": 1027,
+        "publisher": { "@id": "https://privacy.zi-ping.com" },
+        "image": {
+          "@id": "https://blog.zi-ping.com/wp-content/uploads/2023/10/love-blue.png"
+  
+        },
+        "thumbnailUrl": "https://blog.zi-ping.com/wp-content/uploads/2023/10/love-blue.png",
+        "articleSection":  [],
+        "inLanguage": "en-US",
+          "potentialAction": [
+              {
+              "@type": "ReadAction",
+              "target": "https://ziping-liu-corporation.github.io/react-adobe-embed/#/test"
+              }
+          ],
+          "description": "Testing the React Component over Time as the Continuous Form of Avaiability Monitoring Concerning the React Adobe Embed custom react component library Due to possible failiures caused by external API changes ( Adobe Embed API is complicated and changes without notice often) Due to possible failures caused by code changes to the React Component that are unable to be caught through unit tests",
+          "keywords": [ ${topTenWords.map((word) => `"${word.word}"`).join(', ')} ],
+          "commentCount": 0    }
+    ]
+  }
+  </script>	
+
+`;
+
+
+
+
+ 
+
+
+const htmlNew = publicIndexhtml.replace(regexMatchHead, headMatchedModded);
+
+const htmlMiny = minify(htmlNew, {
+    collapseWhitespace: true,
+    minifyCSS: true,
+    minifyJS: true,
+    removeComments: true,
+    removeTagWhitespace: true,
+    preserveLineBreaks: false,
+    minifyJS: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    removeTagWhitespace: true,
+    useShortDoctype: true,
+});
+
+
+fs.writeFileSync(path.resolve(__dirname, '../build/index.html'), htmlMiny, 'utf8');
+
